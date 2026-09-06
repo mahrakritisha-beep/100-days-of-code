9bb89acc2f2c1f13a107c0a89b5240f24d82a861
mahrakritisha-beep <mahrakritisha@gmail.com>
2026-09-06 13:43:57 +0530

    Add files via upload

---
+++ DAY28.C/day28_2.c
@@ -0,0 +1,20 @@
+//Read and print elements of a one-dimensional array.
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+
+    scanf("%d", &n);
+
+    // Read array elements
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Print array elements
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
